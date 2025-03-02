eadab2579e198f1687f606b83087da1e1b1d60c7
bruno vianna <bruno@pobox.com>
2025-03-02 18:22:03 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,28 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Happy)
+})
+input.onButtonPressed(Button.B, function () {
+    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Dadadadum), music.PlaybackMode.InBackground)
+})
+let strip = neopixel.create(DigitalPin.P0, 3, NeoPixelMode.RGB)
+basic.forever(function () {
+    basic.showString("martin")
+    basic.showIcon(IconNames.Sword)
+})
+basic.forever(function () {
+    strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+    strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+    strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+    strip.show()
+    basic.pause(1000)
+    strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+    strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Yellow))
+    strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+    strip.show()
+    basic.pause(1000)
+    strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+    strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+    strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
+    strip.show()
+    basic.pause(1000)
+})
